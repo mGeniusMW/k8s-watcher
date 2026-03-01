7199c30cdaa5406cb45d1061df04a186f198400d
marvin.weiss <marvin.weiss@stud.th-luebeck.de>
2026-03-01 13:28:06 +0100

    Add .gitignore and update go.mod with required dependencies
    Implement LocalPod struct and enhance pod event handling in main.go

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/mGeniusMW/k8s-watcher
 go 1.25.0
 
 require (
+	k8s.io/api v0.35.2
+	k8s.io/client-go v0.35.2
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.12.2 // indirect
 	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
@@ -34,9 +39,7 @@ require (
 	gopkg.in/evanphx/json-patch.v4 v4.13.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.35.2 // indirect
 	k8s.io/apimachinery v0.35.2 // indirect
-	k8s.io/client-go v0.35.2 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20250910181357-589584f1c912 // indirect
 	k8s.io/utils v0.0.0-20251002143259-bc988d571ff4 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"time"
 
+	k8sApi "k8s.io/api/core/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
@@ -12,19 +13,53 @@ import (
 	"k8s.io/client-go/util/homedir"
 )
 
+type LocalPod struct {
+	Name              string
+	Namespace         string
+	Status            string
+	ContainerStatuses []string
+}
+
 func main() {
 	config, _ := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
 	clientset, _ := kubernetes.NewForConfig(config)
 
 	factory := informers.NewSharedInformerFactory(clientset, time.Second*30)
-	
-	podInformer := factory.Core().V1().Pods().Informer()
 
-	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	LocalPodInformer := factory.Core().V1().Pods().Informer()
+	// pod.Status.ContainerStatuses is where info such as CrashloopBackOff etc is stored
+	LocalPodInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			fmt.Printf("NEW POD DETECTED: %v\n", obj)
-		},
-		DeleteFunc: func(obj interface{}) {
+			k8sPod, ok := obj.(*k8sApi.Pod)
+			if !ok {
+				return
+			}
+
+			// 1. Create a slice to hold our strings
+			var statuses []string
+
+			// 2. Loop through the K8s container statuses
+			for _, cs := range k8sPod.Status.ContainerStatuses {
+				// We check if the container is waiting (this is where CrashLoopBackOff lives)
+				if cs.State.Waiting != nil {
+					statuses = append(statuses, cs.State.Waiting.Reason)
+				} else if cs.State.Running != nil {
+					statuses = append(statuses, "Running")
+				} else if cs.State.Terminated != nil {
+					statuses = append(statuses, cs.State.Terminated.Reason)
+				}
+			}
+
+			myLocalPod := LocalPod{
+				Name:              k8sPod.Name,
+				Namespace:         k8sPod.Namespace,
+				Status:            string(k8sPod.Status.Phase),
+				ContainerStatuses: statuses, // Now the types match! ([]string)
+			}
+
+			fmt.Printf("POD ADDED: %s/%s - Status: %s - Container States: %v\n",
+				myLocalPod.Namespace, myLocalPod.Name, myLocalPod.Status, myLocalPod.ContainerStatuses)
+		}, DeleteFunc: func(obj interface{}) {
 			fmt.Printf("POD DELETED: %v\n", obj)
 		},
 	})
@@ -34,6 +69,6 @@ func main() {
 	factory.Start(stop)
 
 	fmt.Println("Watcher started... waiting for changes in 'kind' cluster.")
-	
-	select {} 
+
+	select {}
 }
