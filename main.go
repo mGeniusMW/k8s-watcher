a641b4c7bf890d7a982445cfa95f8757a9eaf481
marvin.weiss <marvin.weiss@stud.th-luebeck.de>
2026-03-01 13:45:06 +0100

    Refactor pod handling and alerting logic in main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -14,53 +15,145 @@ import (
 )
 
 type LocalPod struct {
-	Name              string
-	Namespace         string
+	Name              string 'json:"name"'
+	Namespace         string 
 	Status            string
 	ContainerStatuses []string
 }
 
+func PodAge(pod *k8sApi.Pod) time.Duration {
+	if pod == nil {
+		return 0
+	}
+	return time.Since(pod.CreationTimestamp.Time)
+}
+
+func FormatAgeShort(age time.Duration) string {
+	if age < 0 {
+		age = 0
+	}
+
+	seconds := int(age.Seconds())
+	if seconds < 60 {
+		return fmt.Sprintf("%ds", seconds)
+	}
+
+	minutes := int(age.Minutes())
+	if minutes < 60 {
+		return fmt.Sprintf("%dm", minutes)
+	}
+
+	hours := int(age.Hours())
+	if hours < 24 {
+		return fmt.Sprintf("%dh", hours)
+	}
+
+	days := hours / 24
+	if days < 365 {
+		return fmt.Sprintf("%dd", days)
+	}
+
+	years := days / 365
+	return fmt.Sprintf("%dy", years)
+}
+
+func ExtractPod(obj interface{}) (*k8sApi.Pod, bool) {
+	switch typedObj := obj.(type) {
+	case *k8sApi.Pod:
+		return typedObj, true
+	case cache.DeletedFinalStateUnknown:
+		pod, ok := typedObj.Obj.(*k8sApi.Pod)
+		return pod, ok
+	case *cache.DeletedFinalStateUnknown:
+		pod, ok := typedObj.Obj.(*k8sApi.Pod)
+		return pod, ok
+	default:
+		return nil, false
+	}
+}
+
+func BuildLocalPod(k8sPod *k8sApi.Pod) LocalPod {
+	var statuses []string
+
+	for _, cs := range k8sPod.Status.ContainerStatuses {
+		if cs.State.Waiting != nil {
+			statuses = append(statuses, cs.State.Waiting.Reason)
+		} else if cs.State.Running != nil {
+			statuses = append(statuses, "Running")
+		} else if cs.State.Terminated != nil {
+			statuses = append(statuses, cs.State.Terminated.Reason)
+		}
+	}
+
+	return LocalPod{
+		Name:              k8sPod.Name,
+		Namespace:         k8sPod.Namespace,
+		Status:            string(k8sPod.Status.Phase),
+		ContainerStatuses: statuses,
+	}
+}
+
+func AlertIfNeeded(event string, k8sPod *k8sApi.Pod) {
+	rawAge := PodAge(k8sPod)
+	if rawAge < 30*time.Second {
+		return
+	}
+
+	myLocalPod := BuildLocalPod(k8sPod)
+	if myLocalPod.Status == "Running" {
+		return
+	}
+
+	ageText := FormatAgeShort(rawAge)
+	fmt.Printf("Fire Alert!! %s %s/%s - Age: %s - Status: %s - Container States: %v\n",
+		event, myLocalPod.Namespace, myLocalPod.Name, ageText, myLocalPod.Status, myLocalPod.ContainerStatuses)
+}
+
 func main() {
-	config, _ := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
-	clientset, _ := kubernetes.NewForConfig(config)
+	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load kubeconfig: %v\n", err)
+		os.Exit(1)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create Kubernetes client: %v\n", err)
+		os.Exit(1)
+	}
 
 	factory := informers.NewSharedInformerFactory(clientset, time.Second*30)
 
 	LocalPodInformer := factory.Core().V1().Pods().Informer()
-	// pod.Status.ContainerStatuses is where info such as CrashloopBackOff etc is stored
 	LocalPodInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			k8sPod, ok := obj.(*k8sApi.Pod)
+			k8sPod, ok := ExtractPod(obj)
+			if !ok {
+				return
+			}
+			AlertIfNeeded("POD ADDED:", k8sPod)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			newPod, ok := ExtractPod(newObj)
 			if !ok {
 				return
 			}
 
-			// 1. Create a slice to hold our strings
-			var statuses []string
-
-			// 2. Loop through the K8s container statuses
-			for _, cs := range k8sPod.Status.ContainerStatuses {
-				// We check if the container is waiting (this is where CrashLoopBackOff lives)
-				if cs.State.Waiting != nil {
-					statuses = append(statuses, cs.State.Waiting.Reason)
-				} else if cs.State.Running != nil {
-					statuses = append(statuses, "Running")
-				} else if cs.State.Terminated != nil {
-					statuses = append(statuses, cs.State.Terminated.Reason)
-				}
+			oldPod, ok := ExtractPod(oldObj)
+			if ok && oldPod.ResourceVersion == newPod.ResourceVersion {
+				return
 			}
 
-			myLocalPod := LocalPod{
-				Name:              k8sPod.Name,
-				Namespace:         k8sPod.Namespace,
-				Status:            string(k8sPod.Status.Phase),
-				ContainerStatuses: statuses, // Now the types match! ([]string)
+			AlertIfNeeded("POD UPDATED:", newPod)
+		},
+		DeleteFunc: func(obj interface{}) {
+			k8sPod, ok := ExtractPod(obj)
+			if !ok {
+				fmt.Printf("POD DELETED: unknown object type %T\n", obj)
+				return
 			}
 
-			fmt.Printf("POD ADDED: %s/%s - Status: %s - Container States: %v\n",
-				myLocalPod.Namespace, myLocalPod.Name, myLocalPod.Status, myLocalPod.ContainerStatuses)
-		}, DeleteFunc: func(obj interface{}) {
-			fmt.Printf("POD DELETED: %v\n", obj)
+			fmt.Printf("POD DELETED: %s/%s\n", k8sPod.Namespace, k8sPod.Name)
 		},
 	})
 
